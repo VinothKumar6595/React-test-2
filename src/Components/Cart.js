d363c7b4889c02b93f994da159b0b335ff3b0aea
VinothKumar6595 <vinothcassillas@gmail.com>
2023-08-31 18:54:42 +0530

    Updated working cart

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -3,12 +3,26 @@ import Modal from "./Modal";
 import CartContext from "../Store/Cart-Context";
 const Cart = (props) => {
   const cartCntx = useContext(CartContext);
+  const cartItems = cartCntx.cartItems.map((item) => {
+    const price = item.price * item.L * item.M * item.S;
+    return (
+      <li key={item.id}>
+        <span>{item.name}</span>
+        <span>L:{item.L}</span>
+        <span>M:{item.M}</span>
+        <span>S:{item.S}</span>
+        <span>price:{price}</span>
+      </li>
+    );
+  });
 
+  console.log(numberOfCartItems);
   return (
     <Modal onClick={props.onHideCart}>
+      {cartItems}
       <div>
         <span>Total Amount</span>
-        <span>0</span>
+        <span>{numberOfCartItems}</span>
       </div>
       <div>
         <button>Place Order</button>
